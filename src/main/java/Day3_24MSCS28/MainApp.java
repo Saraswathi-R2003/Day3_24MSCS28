c4a77b3ef2180fbf08a22bdb0827b128f35f07d4
Saraswathi-R2003 <24mscs28@kristujayanti.com>
2025-06-19 10:02:34 +0530

    adding img

--- src/main/java/Day3_24MSCS28/MainApp.java
+++ src/main/java/Day3_24MSCS28/MainApp.java
@@ -1,6 +1,7 @@
 package Day3_24MSCS28;
 
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
 
@@ -12,7 +13,7 @@ public class MainApp {
         Document course1 = Course.createCourse("Math 101", 4);
 
         Document student2 = Student.createStudent("Kuttal", 23);
-        Document course2= Course.createCourse("English 104", 4);
+        Document course2 = Course.createCourse("English 104", 4);
 
         MongoCollection<Document> students = db.getCollection("students");
         MongoCollection<Document> courses = db.getCollection("courses");
@@ -29,6 +30,32 @@ public class MainApp {
         EnrollmentEmbedded.enrollEmbedded(db, student2, course2);
         EnrollmentReferenced.enrollReferenced(db, student2.getObjectId("_id"), course2.getObjectId("_id"));
 
-        System.out.println("Both embedded and referenced enrollments have been inserted.");
+        System.out.println("Both embedded and referenced enrollments have been inserted.\n");
+
+        // ✅ Print Enrollments clearly
+        MongoCollection<Document> enrollments = db.getCollection("enrollments");
+
+        System.out.println("--- Enrollments Collection ---");
+        try (MongoCursor<Document> cursor = enrollments.find().iterator()) {
+            while (cursor.hasNext()) {
+                Document enrollment = cursor.next();
+                System.out.println("Enrollment ID: " + enrollment.getObjectId("_id"));
+
+                if (enrollment.containsKey("student") && enrollment.containsKey("course")) {
+                    // Embedded format
+                    Document studentDoc = (Document) enrollment.get("student");
+                    Document courseDoc = (Document) enrollment.get("course");
+
+                    System.out.println("Student (Embedded): " + studentDoc.toJson());
+                    System.out.println("Course (Embedded): " + courseDoc.toJson());
+                } else if (enrollment.containsKey("student_id") && enrollment.containsKey("course_id")) {
+                    // Referenced format
+                    System.out.println("Student ID (Referenced): " + enrollment.getObjectId("student_id"));
+                    System.out.println("Course ID (Referenced): " + enrollment.getObjectId("course_id"));
+                }
+
+                System.out.println(); // empty line between entries
+            }
+        }
     }
 }
